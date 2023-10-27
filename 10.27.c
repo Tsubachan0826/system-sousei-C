f36ce9789e60507098c64ac7a796b3297b476894
Tsubachan0826 <148538096+Tsubachan0826@users.noreply.github.com>
2023-10-27 19:20:37 +0900

    Add files via upload

---
+++ 10.27.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, result;
+
+	//1‚Â‚Ì®”‚ğ“ü—Í
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B: ");
+	scanf_s("%d", &a);
+
+	if (a % 2 == 1)
+	{
+		printf("Šï”");
+	}
+	else
+	{
+		printf("‹ô”");
+	}
+
+	return 0;
+
+	
+
+
+
+
+}
