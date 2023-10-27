154d75dc1fb0be63a140960834c5df8c7329a6f7
Tsubachan0826 <148538096+Tsubachan0826@users.noreply.github.com>
2023-10-27 19:43:34 +0900

    課題1

---
+++ ソース.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int a, b, result;
+	
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B: ");
+	scanf("%d", &a);
+	printf("‚à‚¤ˆê‚Â‚Ì”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B: ");
+	scanf("%d", &b);
+
+	result = a * b;
+
+	printf("%d", result);
+	return 0;
+}
